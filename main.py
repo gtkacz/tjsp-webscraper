bf7ec5ac066b9924857e4675e64e03420800906e
Tkacz <gabrielmt2@al.insper.edu.br>
2021-08-12 09:08:58 -0300

    timeout

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests, time
 import pandas as pd
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from tkinter import Tk, ttk, StringVar
+from tkinter import Tk, ttk, StringVar, messagebox
 from pathlib import Path
 from bs4 import BeautifulSoup
 
@@ -15,13 +15,15 @@ def GUI(URL, SEARCH_TERM, CLASS_TERM, START_DATE, END_DATE):
     window.resizable(False, False)
     window.eval('tk::PlaceWindow . center')
     
-    URL=StringVar(URL)
-    SEARCH_TERM=StringVar(SEARCH_TERM)
-    CLASS_TERM=StringVar(CLASS_TERM)
-    START_DATE=StringVar(START_DATE)
-    END_DATE=StringVar(END_DATE)
+    #URL = StringVar(URL)
+    #SEARCH_TERM = StringVar(SEARCH_TERM)
+    #CLASS_TERM = StringVar(CLASS_TERM)
+    #START_DATE = StringVar(START_DATE)
+    #END_DATE = StringVar(END_DATE)
     
     url_tk = ttk.Entry(justify = 'center', exportselection = 0)
+    
+    url_tk.grid(row=2, column=2, padx=25, pady=10)
 
 def process_PDF(text):
     pass
@@ -43,12 +45,12 @@ def main():
     OPTIONS.add_argument('--headless')
     
     try:
-        DRIVER = webdriver.Chrome(PATH, chrome_options=OPTIONS)
+        DRIVER = webdriver.Chrome(PATH, options=OPTIONS)
         DRIVER.get(URL)
         
     except WebDriverException:
         OPTIONS.binary_location = 'D:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-        DRIVER = webdriver.Chrome(PATH, chrome_options=OPTIONS)
+        DRIVER = webdriver.Chrome(PATH, options=OPTIONS)
         DRIVER.get(URL)
     
     SEARCHBAR = DRIVER.find_element_by_id('iddadosConsulta.pesquisaLivre')
@@ -65,13 +67,23 @@ def main():
     CONSULT = DRIVER.find_element_by_id('pbSubmit')
     CONSULT.click()
     
-    PARSEURL = DRIVER.current_url
-    PARSE = requests.request('GET', PARSEURL).text
-    SOUP = BeautifulSoup(PARSE)
     
-    CONTENT = SOUP('table')
-    
-    DRIVER.quit()
+    try:
+        TABLE = WebDriverWait(DRIVER, 10).until(expected_conditions.presence_of_element_located((By.ID, 'divDadosResultado')))
+        PARSEURL = DRIVER.current_url
+        PARSE = requests.request('GET', PARSEURL).text
+        SOUP = BeautifulSoup(PARSE)
+        
+        CONTENT = SOUP('table', features='lxml')
+        
+        DRIVER.quit()
+        
+    except TimeoutException:
+        root = Tk()
+        root.withdraw()
+        messagebox.showerror('Timeout', 'O site demorou demais para responder, tente novamente.')
+        root.destroy()
+        DRIVER.quit()
     
 if __name__ == '__main__':
     main()
