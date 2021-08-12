9e52b897f6de69c28b36580eeb2be4f32deb018a
Tkacz <gabrielmt2@al.insper.edu.br>
2021-08-12 09:25:53 -0300

    continuacao

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-import requests, time
+import urllib.request
 import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from tkinter import Tk, ttk, StringVar, messagebox
+from tkinter import Tk, messagebox
 from pathlib import Path
-from bs4 import BeautifulSoup
-
-def GUI(URL, SEARCH_TERM, CLASS_TERM, START_DATE, END_DATE):
-    window = Tk()
-    window.title('TJSP Webscrapper')
-    window.resizable(False, False)
-    window.eval('tk::PlaceWindow . center')
-    
-    #URL = StringVar(URL)
-    #SEARCH_TERM = StringVar(SEARCH_TERM)
-    #CLASS_TERM = StringVar(CLASS_TERM)
-    #START_DATE = StringVar(START_DATE)
-    #END_DATE = StringVar(END_DATE)
-    
-    url_tk = ttk.Entry(justify = 'center', exportselection = 0)
-    
-    url_tk.grid(row=2, column=2, padx=25, pady=10)
-
-def process_PDF(text):
-    pass
+from utils import *
 
 def main():
     CUR_DIR = Path(__file__).parent
@@ -67,22 +48,20 @@ def main():
     CONSULT = DRIVER.find_element_by_id('pbSubmit')
     CONSULT.click()
     
-    
     try:
         TABLE = WebDriverWait(DRIVER, 10).until(expected_conditions.presence_of_element_located((By.ID, 'divDadosResultado')))
         PARSEURL = DRIVER.current_url
-        PARSE = requests.request('GET', PARSEURL).text
-        SOUP = BeautifulSoup(PARSE)
         
-        CONTENT = SOUP('table', features='lxml')
-        
-        DRIVER.quit()
+        with urllib.request.urlopen(urllib.request.Request(PARSEURL, headers={'User-Agent': 'Chrome'})) as HTML:
+            PAGE = HTML.read()
         
     except TimeoutException:
         root = Tk()
         root.withdraw()
         messagebox.showerror('Timeout', 'O site demorou demais para responder, tente novamente.')
         root.destroy()
+        
+    finally:
         DRIVER.quit()
     
 if __name__ == '__main__':
